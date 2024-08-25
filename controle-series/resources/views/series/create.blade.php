58890227158c800cca29f9fca983ec1383c47186
Marcos Paulo <marcos.poo@gmail.com>
2024-08-25 20:45:39 -0300

    Aula 5.3 - DB Facade

--- controle-series/resources/views/series/create.blade.php
+++ controle-series/resources/views/series/create.blade.php
@@ -1,7 +1,8 @@
 <x-layout title="Nova Série">
 <a href="/series" class="btn btn-dark mb-2">Voltar</a>
 
-    <form action="" method="post">
+    <form action="/series/salvar" method="post">
+        @csrf
         <div class="mb-3">
             <label  for="nome" class="form-label">Nome:</label>
             <input type="text" id="nome" name="nome" class="form-control">
